627ad56a699701eb4d4c7c0aa1ca7207ac50eb6a
MathisSenicourt <mathiscience@hotmail.fr>
2024-06-20 12:30:17 +0200

    link db for showroom

--- src/app/pages/showroom/showroom.component.ts
+++ src/app/pages/showroom/showroom.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {CarsService} from "../../services/cars.service";
 
 @Component({
   selector: 'app-showroom',
@@ -11,7 +12,12 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './showroom.component.html',
   styleUrl: './showroom.component.css'
 })
-export class ShowroomComponent {
+export class ShowroomComponent implements OnInit {
+
+  constructor(
+    private carsService: CarsService,
+  ) {}
+
   brands: string[] = ['Toyota', 'Honda', 'Ford', 'BMW', 'Audi'];
   selectedBrands: string[] = [];
   priceMin: number = 0;
@@ -24,21 +30,21 @@ export class ShowroomComponent {
   yearMaxValue: number = new Date().getFullYear();
 
   cars: any[] = [
-    { 
+    {
       model: 'Camry',
       brand: 'Toyota',
       year: 2020,
       km: 15000,
       price: 25000
     },
-    { 
+    {
       model: 'Accord',
       brand: 'Honda',
       year: 2019,
       km: 18000,
       price: 22000
     },
-    { 
+    {
       model: 'Mustang',
       brand: 'Ford',
       year: 2021,
@@ -46,6 +52,15 @@ export class ShowroomComponent {
       price: 40000
     }
   ];
+
+  ngOnInit() {
+    this.carsService.getAllCars()
+      .subscribe((data) => {
+        this.cars = data;
+        console.log(this.cars)
+      });
+    console.log(this.cars)
+  }
 
   onCheckboxChange(event: any) {
     const brand = event.target.value;
@@ -57,7 +72,7 @@ export class ShowroomComponent {
         this.selectedBrands.splice(index, 1);
       }
     }
-    console.log(this.selectedBrands); 
+    console.log(this.selectedBrands);
   }
 
   onPriceChange() {
